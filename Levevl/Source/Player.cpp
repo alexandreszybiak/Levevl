b1547447d4db9244a1fc298294b1e63925d2326e
Alexandre Szybiak <alexandre.szybiak@gmail.com>
2022-07-26 21:59:57 +0200

    Added basic sprite animation system

--- Levevl/Source/Player.cpp
+++ Levevl/Source/Player.cpp
@@ -1,10 +1,24 @@
+#include "SDL.h"
 #include "Player.h"
 #include "Graphics.h"
 
-Player::Player(int x, int y) : m_x(x), m_y(y), m_destinationRect({0,0,26,39}) {}
+Player::Player(int x, int y) :
+m_x(x),
+m_y(y),
+m_destinationRect({0,0,40,48}),
+m_currentFrame(0),
+m_lastFrameTime(0)
+{
+	
+}
 
 void Player::Draw(Graphics& graphics) {
-	SDL_Rect srcRect = { 0,0,26,39 };
+	if (SDL_GetTicks() - m_lastFrameTime > m_frameDuration) {
+		m_currentFrame = (m_currentFrame + 1) % 4;
+		m_lastFrameTime = SDL_GetTicks();
+	}
+
+	SDL_Rect srcRect = { 40 + 40 * m_currentFrame,0,40,48 };
 
 	m_destinationRect.x = m_x;
 	m_destinationRect.y = m_y;
--- Levevl/Source/Player.h
+++ Levevl/Source/Player.h
@@ -10,6 +10,13 @@ private:
 	int m_x, m_y;
 	SDL_Rect m_destinationRect;
 
+	int m_currentFrame;
+
+	// The frame duration in milliseconds
+	const int m_frameDuration = 83;
+
+	int m_lastFrameTime;
+
 public:
 	Player(int x, int y);
 	void Draw(Graphics& graphics);
