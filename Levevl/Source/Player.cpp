351d6a01b88f5ff5df02a3330cbb2fa29b54ce3e
Alexandre Szybiak <alexandre.szybiak@gmail.com>
2022-07-28 22:08:26 +0200

    Added simple animations

--- Levevl/Source/Player.cpp
+++ Levevl/Source/Player.cpp
@@ -1,31 +1,47 @@
+#include <algorithm>
 #include "SDL.h"
 #include "Player.h"
 #include "Graphics.h"
 #include "Input.h"
 
 Player::Player(int x, int y) : m_x(x), m_y(y), m_direction(DIRECTION_RIGHT), m_destinationRect({0,0,40,48}),
-	m_currentFrame(0), m_lastFrameTime(0) {
+	m_currentFrame(0), m_lastFrameTime(0), m_currentAnimation(nullptr) {
+
+	m_idleAnimation.reserve(1);
+	m_idleAnimation = { 0 };
+
+	m_runAnimation.reserve(4);
+	m_runAnimation = { 1, 2, 3, 4 };
+
+	PlayAnimation(&m_idleAnimation);
 	
 }
 
 void Player::Update(Input& input) {
 	int hDir = -input.IsKeyHeld(SDL_SCANCODE_LEFT) + input.IsKeyHeld(SDL_SCANCODE_RIGHT);
 
-	if (hDir > 0)
-		m_direction = DIRECTION_RIGHT;
-	else if (hDir < 0)
-		m_direction = DIRECTION_LEFT;
+	if (hDir != 0) {
+		PlayAnimation(&m_runAnimation);
+		if (hDir > 0)
+			m_direction = DIRECTION_RIGHT;
+		else if (hDir < 0)
+			m_direction = DIRECTION_LEFT;
+	}
+	else {
+		PlayAnimation(&m_idleAnimation);
+	}
 
 	m_x += hDir * 2;
 }
 
 void Player::Draw(Graphics& graphics) {
 	if (SDL_GetTicks() - m_lastFrameTime > m_frameDuration) {
-		m_currentFrame = (m_currentFrame + 1) % 4;
+		m_animationIterator = (m_animationIterator + 1) % m_currentAnimation->size();
+		m_currentFrame = (* m_currentAnimation)[m_animationIterator];
 		m_lastFrameTime = SDL_GetTicks();
 	}
 
-	SDL_Rect srcRect = { 40 + 40 * m_currentFrame,0,40,48 };
+	SDL_Rect srcRect = { 40 * m_currentFrame,0,40,48 };
 
 	m_destinationRect.x = m_x;
 	m_destinationRect.y = m_y;
@@ -42,4 +58,8 @@ void Player::Draw(Graphics& graphics) {
 void Player::SetPosition(int x, int y) {
 	m_x = x;
 	m_y = y;
+}
+
+void Player::PlayAnimation(std::vector<Uint8>* animation) {
+	m_currentAnimation = animation;
 }
--- Levevl/Source/Player.h
+++ Levevl/Source/Player.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include "SDL.h"
 
 class Graphics;
@@ -19,16 +20,27 @@ private:
 
 	SDL_Rect m_destinationRect;
 
+	int m_animationIterator;
+
 	int m_currentFrame;
+
+	std::vector<Uint8>* m_currentAnimation;
 
 	// The frame duration in milliseconds
 	const int m_frameDuration = 83;
 
 	int m_lastFrameTime;
 
+	// Idle animation
+	std::vector<Uint8> m_idleAnimation;
+
+	// Run animation
+	std::vector<Uint8> m_runAnimation;
+
 public:
 	Player(int x, int y);
 	void Update(Input& input);
 	void Draw(Graphics& graphics);
 	void SetPosition(int x, int y);
+	void PlayAnimation(std::vector<Uint8>* animation);
 };
